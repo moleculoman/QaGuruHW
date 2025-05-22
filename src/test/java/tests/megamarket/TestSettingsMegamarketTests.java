dd42870d2be1cd1b8f683296a20434d5ff7a076e
moleculoman <115730179+moleculoman@users.noreply.github.com>
2025-05-22 21:10:49 +0700

    add capabilities to MMT

--- src/test/java/tests/megamarket/TestSettingsMegamarketTests.java
+++ src/test/java/tests/megamarket/TestSettingsMegamarketTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -15,7 +16,7 @@ public class TestSettingsMegamarketTests {
         Configuration.pageLoadStrategy = "eager";
         //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
+        Configuration.browserCapabilities = new DesiredCapabilities();
     }
     public TestSettingsMegamarketTests openPage() {
         open("");
