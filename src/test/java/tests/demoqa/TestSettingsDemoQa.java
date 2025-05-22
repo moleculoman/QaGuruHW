4c803b4d0a4ed6528d12564dbf0f71cc6d9030ec
moleculoman <115730179+moleculoman@users.noreply.github.com>
2025-05-22 21:28:09 +0700

    addAttachments added

--- src/test/java/tests/demoqa/TestSettingsDemoQa.java
+++ src/test/java/tests/demoqa/TestSettingsDemoQa.java
@@ -3,8 +3,10 @@ package tests.demoqa;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import utils.Attach;
 
 import java.util.Map;
 
@@ -24,5 +26,14 @@ public class TestSettingsDemoQa {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
     }
 }
