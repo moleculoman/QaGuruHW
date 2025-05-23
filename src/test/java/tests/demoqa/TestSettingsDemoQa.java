9795604723146ad765ef9cc6ff79fc8b57140fac
moleculoman <115730179+moleculoman@users.noreply.github.com>
2025-05-23 08:22:09 +0700

    add system.properties for DemoQaTests

--- src/test/java/tests/demoqa/TestSettingsDemoQa.java
+++ src/test/java/tests/demoqa/TestSettingsDemoQa.java
@@ -13,10 +13,14 @@ import java.util.Map;
 public class TestSettingsDemoQa {
     @BeforeAll
     static void settingsForBrowserDemoQa() {
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = System.getProperty("browser.size", "1920x1080");        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser.version", "128.0");
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        final String SELENOID_URL = System.getProperty("selenoid.url");
+        final String SELENOID_LOGIN = System.getProperty("selenoid.login");;
+        final String SELENOID_PASSWORD = System.getProperty("selenoid.password");
+        Configuration.remote = "https://" + SELENOID_LOGIN + ":" + SELENOID_PASSWORD + "@" + SELENOID_URL + "/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
