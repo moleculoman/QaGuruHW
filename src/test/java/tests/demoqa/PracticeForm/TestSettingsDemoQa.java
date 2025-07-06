8f36bf9399c9b2a65b93f3ff908716d5d6d827a9
moleculoman <115730179+moleculoman@users.noreply.github.com>
2025-07-06 21:20:14 +0700

    refactoring due to comments

--- src/test/java/tests/demoqa/PracticeForm/TestSettingsDemoQa.java
+++ src/test/java/tests/demoqa/PracticeForm/TestSettingsDemoQa.java
@@ -4,30 +4,39 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.LoggerFactory;
 import helpers.Attach;
-
-import java.util.Map;
 import org.slf4j.Logger;
-import tests.demoqa.PracticeForm.config.ConfigReader;
-import tests.demoqa.PracticeForm.config.ProjectConfiguration;
 import tests.demoqa.PracticeForm.config.web.WebConfig;
-
-import static com.codeborne.selenide.Selenide.open;
 
 public class TestSettingsDemoQa {
     private static final Logger logger = LoggerFactory.getLogger(TestSettingsDemoQa.class);
-    private static final WebConfig webConfig = ConfigReader.Instance.read();
+    private static final WebConfig webConfig = ConfigFactory.create(WebConfig.class, System.getProperties());
 
     @BeforeAll
     public static void setUp() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-        ProjectConfiguration projectConfiguration = new ProjectConfiguration(webConfig);
-        projectConfiguration.webConfig();
+        Configuration.baseUrl = webConfig.baseUrl();
+        Configuration.browser = webConfig.browser().toString();
+        Configuration.browserVersion = webConfig.browserVersion();
+        Configuration.browserSize = webConfig.browserSize();
+        Configuration.pageLoadStrategy = "eager";
+        if (webConfig.isRemote()) {
+            Configuration.remote = webConfig.remoteUrl();
+
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            //capabilities.setCapability("enableVNC", true);
+            //capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+
+            if (webConfig.remoteUrl() == null || webConfig.remoteUrl().isEmpty()) {
+                throw new IllegalStateException("Remote URL is not configured or is empty");
+            }
+        }
     }
 
     @AfterEach
@@ -37,5 +46,10 @@ public class TestSettingsDemoQa {
         Attach.browserConsoleLogs();
         Attach.addVideo();
         Selenide.closeWebDriver();
+    }
+
+    @BeforeEach
+    void addListener(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 }
